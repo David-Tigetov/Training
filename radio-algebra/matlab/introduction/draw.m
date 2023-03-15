0db9b9f68371c5bb98a430f4a8c9a009596d4279
Tigetov David <dg.tigetov@ocelot.ar003.ru>
2023-03-15 15:24:56 +0300

    radio-algebra: для отношения Релея добавлены примеры.

--- radio-algebra/matlab/introduction/draw.m
+++ radio-algebra/matlab/introduction/draw.m
@@ -1,19 +1,22 @@
 function draw ( axes, path, curve )
 %DRAW изображение кривой
 
-    % находим индексы минимального и максимального значения на кривой
+        % находим индексы минимального и максимального значения на кривой
     [ ~, minimum ] = min ( curve );
     [ ~, maximum ] = max ( curve );
 
     hold on
+
     % путь
     plot3 ( axes, path ( 1, : ), path ( 2, : ), zeros ( 1, size ( path, 2 ) ), 'b' );
     % кривая
     plot3 ( axes, path ( 1, : ), path ( 2, : ), curve, 'r' );
+
     % точка мимимума
-    plot3 ( axes, [ 0 , path( 1, minimum ) ], [ 0, path( 2, minimum ) ], [ 0, 0 ], 'b' );
+    plot3 ( axes, [ -path( 1, minimum ), path( 1, minimum ) ], [ -path( 2, minimum ), path( 2, minimum ) ], [ 0, 0 ], 'm' );
     % точка максимума
-    plot3 ( axes, [ 0 , path( 1, maximum ) ], [ 0, path( 2, maximum ) ], [ 0, 0 ], 'b' );
+    plot3 ( axes, [ -path( 1, maximum ), path( 1, maximum ) ], [ -path( 2, maximum ), path( 2, maximum ) ], [ 0, 0 ], 'c' );
+
     hold off
     grid on
 end
