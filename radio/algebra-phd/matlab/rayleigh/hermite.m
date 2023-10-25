546b4420a23c3c73a010ff9ee70e7bf469d466e5
Tigetov David <dg.tigetov@ocelot.ar003.ru>
2023-10-25 11:25:56 +0300

    radio-algebra: добавлены задания к зачёту по отношению Релея.

--- radio/algebra-phd/matlab/rayleigh/hermite.m
+++ radio/algebra-phd/matlab/rayleigh/hermite.m
@@ -1,18 +1,15 @@
-function H = hermite( definition )
+function H = hermite( size, definition )
 %HERMITE формирование эрмитовых матриц
 
     % формируем произвольную матрицу
-    U = rand ( 2, 2 );
-    % ортогонализуем второй столбец
-    U(:,2) = U(:,2) - dot( U(:,1), U(:,2) ) * U(:,1) / norm( U(:,1) )^2;
-    % нормируем столбцы
-    U(:,1) = U(:,1)/norm(U(:,1));
-    U(:,2) = U(:,2)/norm(U(:,2));
+    U = rand ( size, size ) + 1i * rand ( size, size );
+    % ортогонализируем и нормируем столбцы
+    U = orth ( U );
 
     % формируем собственные числа
-    diagonal = ( rand ( 1, 2 ) - 0.5 ) * 10;
+    diagonal = ( rand ( 1, size ) - 0.5 ) * 10;
     if ( definition == "positive" )
-        diagonal = abs ( diagonal );
+        diagonal = abs ( diagonal ) + 1;
     end
     % формируем диагональную матрицу
     D = diag ( diagonal );
